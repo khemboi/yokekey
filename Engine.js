7d89a11dd5d59692f364229601301c4ea7051b9e
Khem Boi <basedgodtotodile@gmail.com>
2021-12-31 20:04:06 -0500

    Add files via upload

---
+++ Engine.js
@@ -0,0 +1,15 @@
+document.getElementById('FileMaker').addEventListener('keydown', function(a) {
+  if (a.key == 'Tab') {
+    a.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) +
+      "\t" + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;
+  }
+});
